db137bb75bdbf654ee8a5182c0069db0c8c0ed70
Kacper_K <113976235+C00271692@users.noreply.github.com>
2024-10-11 14:18:57 +0100

    first commit

---
+++ KrakowiakKacperTestTask1.java
@@ -0,0 +1,11 @@
+// Student Name:
+// Student Id:
+// Description: Junit Black box test case for Rate and Period
+
+package cm;
+
+public class KrakowiakKacperTestTask1 {
+    public static void main (String[] args) {
+        System.out.println("Krakowiak Kacper");
+    }
+}
